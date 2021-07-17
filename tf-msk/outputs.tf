823ada69b7dba6ee8feb55cfc2fa0e729f68e2fc
Gary A. Stafford <gary.a.stafford@gmail.com>
2021-07-17 08:18:04 -0400

    Fix tf outputs

--- tf-msk/outputs.tf
+++ tf-msk/outputs.tf
@@ -1,19 +1,29 @@
-output "zookeeper_connect_string" {
+output "zookeeper_connect_string_no_iam" {
   description = "Apache ZooKeeper connection"
   value       = aws_msk_cluster.msk_cluster.zookeeper_connect_string
 }
 
-output "bootstrap_brokers_tls" {
+output "bootstrap_brokers_tls_no_iam" {
   description = "TLS connection host:port pairs"
   value       = aws_msk_cluster.msk_cluster.bootstrap_brokers_tls
 }
 
 output "zookeeper_connect_string_iam" {
-  description = "Apache ZooKeeper connection"
+  description = "Apache ZooKeeper connection IAM"
   value       = aws_msk_cluster.msk_cluster_iam.zookeeper_connect_string
 }
 
 output "bootstrap_brokers_sasl_iam" {
   description = "SASL IAM connection host:port pairs"
   value       = aws_msk_cluster.msk_cluster_iam.bootstrap_brokers_sasl_iam
+}
+
+output "zookeeper_connect_string_scram" {
+  description = "Apache ZooKeeper connection ISCRAM"
+  value       = aws_msk_cluster.msk_cluster_scram.zookeeper_connect_string
+}
+
+output "bootstrap_brokers_sasl_scram" {
+  description = "SASL SCRAM connection host:port pairs"
+  value       = aws_msk_cluster.msk_cluster_scram.bootstrap_brokers_sasl_scram
 }
